d921ec9655da5e03b1b67df8a0ffc90375cf8e1a
Gea-Suan Lin <gslin@gslin.org>
2016-10-11 21:34:44 +0800

    Update build.sh.

--- build.sh
+++ build.sh
@@ -3,31 +3,48 @@
 TMPDIR=/tmp/netdata
 URL_PREFIX=https://github.com/firehol/netdata/releases/download
 
+TMPDIR="/tmp/${NAME}"
+
 if [[ "x$1" = "x" ]]; then
     cat <<EOF
 Usage:
-    $0 <version>
+    $0 <tag or hash> [version name]
+
+Example:
+    $0 0.2.0
+    $0 6192b33 0.2.0.20160822
 EOF
     exit
 else
-    VERSION="$1"
+    GIT_HASH="$1"
 fi
 
-BASEDIR="${TMPDIR}/netdata-${VERSION}"
-TARBALL="netdata-${VERSION}.tar.gz"
-URL="${URL_PREFIX}/v${VERSION}/${TARBALL}"
+if [[ "x$2" = "x" ]]; then
+    VERSION="$1"
+else
+    VERSION="$2"
+fi
+
+BASEDIR="${TMPDIR}/${NAME}-${VERSION}"
+TARBALL="${NAME}-${VERSION}.tar.gz"
 
 rm -rf -- "${TMPDIR}"
 mkdir -p "${TMPDIR}"
 
-wget -c -O "${TMPDIR}/${TARBALL}" -- "${URL}"
-tar -zxv -C "${TMPDIR}" -f "${TMPDIR}/${TARBALL}"
+pushd "${TMPDIR}/"
+git clone "${GIT_REPOSITORY_URL}" "${BASEDIR}/"
+cd "${BASEDIR}/"
+git checkout "${GIT_HASH}"
+cd ..
+tar -zcv --exclude-vcs -f "${TARBALL}" "${NAME}-${VERSION}/"
+popd
+
 cp -R debian/ "${BASEDIR}/"
 pushd "${BASEDIR}/"
 dh_make -f "../${TARBALL}" -s < /dev/null
 
 # If we have already submitted this version before, use -i to increase version.
-if grep -q "^netdata (${VERSION}" debian/changelog; then
+if grep -q "^${NAME} (${VERSION}" debian/changelog; then
     dch --distribution unstable -i
 else
     dch --distribution unstable -v "${VERSION}-1ubuntu1~unstable~ppa1"
